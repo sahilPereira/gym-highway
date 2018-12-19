cc784e04d5a490553954743254b8ff55834807e5
Sahil Pereira <s6pereira@uwaterloo.ca>
2018-12-18 19:18:31 -0500

    Update to ICM graph

--- gym_highway/models/icm_graph.py
+++ gym_highway/models/icm_graph.py
@@ -16,82 +16,16 @@ from ray.rllib.models.misc import linear, normc_initializer
 from ray.rllib.utils.explained_variance import explained_variance
 
 
-class PPOLoss(object):
+class ICMLoss(object):
     def __init__(self,
                  action_space,
-                 value_targets,
-                 advantages,
-                 actions,
-                 logits,
-                 vf_preds,
-                 curr_action_dist,
-                 value_fn,
-                 cur_kl_coeff,
                  unsupType,
-                 predictor,
-                 entropy_coeff=0,
-                 clip_param=0.1,
-                 vf_clip_param=0.1,
-                 vf_loss_coeff=1.0,
-                 use_gae=True):
+                 predictor):
         """Constructs the loss for Proximal Policy Objective.
 
         Arguments:
             action_space: Environment observation space specification.
-            value_targets (Placeholder): Placeholder for target values; used
-                for GAE.
-            actions (Placeholder): Placeholder for actions taken
-                from previous model evaluation.
-            advantages (Placeholder): Placeholder for calculated advantages
-                from previous model evaluation.
-            logits (Placeholder): Placeholder for logits output from
-                previous model evaluation.
-            vf_preds (Placeholder): Placeholder for value function output
-                from previous model evaluation.
-            curr_action_dist (ActionDistribution): ActionDistribution
-                of the current model.
-            value_fn (Tensor): Current value function output Tensor.
-            cur_kl_coeff (Variable): Variable holding the current PPO KL
-                coefficient.
-            entropy_coeff (float): Coefficient of the entropy regularizer.
-            clip_param (float): Clip parameter
-            vf_clip_param (float): Clip parameter for the value function
-            vf_loss_coeff (float): Coefficient of the value function loss
-            use_gae (bool): If true, use the Generalized Advantage Estimator.
         """
-        dist_cls, _ = ModelCatalog.get_action_dist(action_space, {})
-        prev_dist = dist_cls(logits)
-        # Make loss functions.
-        logp_ratio = tf.exp(
-            curr_action_dist.logp(actions) - prev_dist.logp(actions))
-        action_kl = prev_dist.kl(curr_action_dist)
-        self.mean_kl = tf.reduce_mean(action_kl)
-
-        curr_entropy = curr_action_dist.entropy()
-        self.mean_entropy = tf.reduce_mean(curr_entropy)
-
-        surrogate_loss = tf.minimum(
-            advantages * logp_ratio,
-            advantages * tf.clip_by_value(logp_ratio, 1 - clip_param,
-                                          1 + clip_param))
-        self.mean_policy_loss = tf.reduce_mean(-surrogate_loss)
-
-        if use_gae:
-            vf_loss1 = tf.square(value_fn - value_targets)
-            vf_clipped = vf_preds + tf.clip_by_value(
-                value_fn - vf_preds, -vf_clip_param, vf_clip_param)
-            vf_loss2 = tf.square(vf_clipped - value_targets)
-            vf_loss = tf.maximum(vf_loss1, vf_loss2)
-            self.mean_vf_loss = tf.reduce_mean(vf_loss)
-            loss = tf.reduce_mean(-surrogate_loss + cur_kl_coeff * action_kl +
-                                  vf_loss_coeff * vf_loss -
-                                  entropy_coeff * curr_entropy)
-        else:
-            self.mean_vf_loss = tf.constant(0.0)
-            loss = tf.reduce_mean(-surrogate_loss + cur_kl_coeff * action_kl -
-                                  entropy_coeff * curr_entropy)
-        self.loss = loss
-
         # computing predictor loss
         self.predloss = None
         if unsupType is not None:
@@ -101,7 +35,7 @@ class PPOLoss(object):
                 self.predloss = constants['PREDICTION_LR_SCALE'] * (predictor.invloss * (1-constants['FORWARD_LOSS_WT']) +
                                                                 predictor.forwardloss * constants['FORWARD_LOSS_WT'])
 
-class PPOPolicyGraphICM(LearningRateSchedule, TFPolicyGraph):
+class ICMGraph(LearningRateSchedule, TFPolicyGraph):
     def __init__(self,
                  observation_space,
                  action_space,
@@ -118,9 +52,6 @@ class PPOPolicyGraphICM(LearningRateSchedule, TFPolicyGraph):
                 placeholders upon which the graph should be built upon.
         """
         self.unsup = unsupType is not None
-        # self.cur_batch = None
-        # self.cur_sample_batch = {}
-
         predictor = None
         numaction = action_space.n
 
@@ -128,117 +59,40 @@ class PPOPolicyGraphICM(LearningRateSchedule, TFPolicyGraph):
         self.sess = tf.get_default_session()
         self.action_space = action_space
         self.config = config
-        self.kl_coeff_val = self.config["kl_coeff"]
-        self.kl_target = self.config["kl_target"]
 
         dist_cls, logit_dim = ModelCatalog.get_action_dist(
             action_space, self.config["model"])
 
-        if existing_inputs:
-            obs_ph, value_targets_ph, adv_ph, act_ph, \
-                logits_ph, vf_preds_ph = existing_inputs[:6]
-            existing_state_in = existing_inputs[6:-1]
-            existing_seq_lens = existing_inputs[-1]
-        else:
-            obs_ph = tf.placeholder(
-                tf.float32,
-                name="obs",
-                shape=(None, ) + observation_space.shape)
-            adv_ph = tf.placeholder(
-                tf.float32, name="advantages", shape=(None, ))
-            act_ph = ModelCatalog.get_action_placeholder(action_space)
-            logits_ph = tf.placeholder(
-                tf.float32, name="logits", shape=(None, logit_dim))
-            vf_preds_ph = tf.placeholder(
-                tf.float32, name="vf_preds", shape=(None, ))
-            value_targets_ph = tf.placeholder(
-                tf.float32, name="value_targets", shape=(None, ))
-            existing_state_in = None
-            existing_seq_lens = None
-        self.observations = obs_ph
+        obs_ph = tf.placeholder(tf.float32, name="obs", shape=(None, ) + observation_space.shape)
+        act_ph = ModelCatalog.get_action_placeholder(action_space)
 
-        self.model = pi = ModelCatalog.get_model(
-            obs_ph,
-            logit_dim,
-            self.config["model"],
-            state_in=existing_state_in,
-            seq_lens=existing_seq_lens)
+        # print("Observations space:", observation_space)
+        print("Observations shape:", tf.shape(self.observations)[0]) # tf.shape(x)[0]
+        print("Observations shape:", self.observations.shape[1:])
+        print("obs_ph shape:", tf.shape(obs_ph)[:1])
+        with tf.variable_scope("predictor"):
+            if 'state' in unsupType:
+                self.local_ap_network = predictor = StatePredictor(tf.shape(self.observations)[0], numaction, designHead, unsupType)
+            else:
+                self.local_ap_network = predictor = StateActionPredictor(tf.shape(self.observations)[0], numaction, designHead)
 
-        if self.unsup:
-            # print("Observations space:", observation_space)
-            print("Observations shape:", tf.shape(self.observations)[0]) # tf.shape(x)[0]
-            print("Observations shape:", self.observations.shape[1:])
-            print("obs_ph shape:", tf.shape(obs_ph)[:1])
-            with tf.variable_scope("predictor"):
-                if 'state' in unsupType:
-                    self.local_ap_network = predictor = StatePredictor(tf.shape(self.observations)[0], numaction, designHead, unsupType)
-                else:
-                    self.local_ap_network = predictor = StateActionPredictor(tf.shape(self.observations)[0], numaction, designHead)
 
-        # KL Coefficient
-        self.kl_coeff = tf.get_variable(
-            initializer=tf.constant_initializer(self.kl_coeff_val),
-            name="kl_coeff",
-            shape=(),
-            trainable=False,
-            dtype=tf.float32)
-
-        self.logits = self.model.outputs
+        self.logits = self.local_ap_network.outputs
         curr_action_dist = dist_cls(self.logits)
         self.sampler = curr_action_dist.sample()
 
-        if self.config["use_gae"]:
-            if self.config["vf_share_layers"]:
-                self.value_function = tf.reshape(
-                    linear(self.model.last_layer, 1, "value",
-                           normc_initializer(1.0)), [-1])
-            else:
-                vf_config = self.config["model"].copy()
-                # Do not split the last layer of the value function into
-                # mean parameters and standard deviation parameters and
-                # do not make the standard deviations free variables.
-                vf_config["free_log_std"] = False
-                vf_config["use_lstm"] = False
-                with tf.variable_scope("value_function"):
-                    self.value_function = ModelCatalog.get_model(
-                        obs_ph, 1, vf_config).outputs
-                    self.value_function = tf.reshape(self.value_function, [-1])
-        else:
-            self.value_function = tf.zeros(shape=tf.shape(obs_ph)[:1])
 
+        self.loss_obj = ICMLoss(action_space, unsupType, predictor)
 
-        self.loss_obj = PPOLoss(
-            action_space,
-            value_targets_ph,
-            adv_ph,
-            act_ph,
-            logits_ph,
-            vf_preds_ph,
-            curr_action_dist,
-            self.value_function,
-            self.kl_coeff,
-            unsupType,
-            predictor,
-            entropy_coeff=self.config["entropy_coeff"],
-            clip_param=self.config["clip_param"],
-            vf_clip_param=self.config["vf_clip_param"],
-            vf_loss_coeff=self.config["vf_loss_coeff"],
-            use_gae=self.config["use_gae"])
-
-        LearningRateSchedule.__init__(self, self.config["lr"],
-                                      self.config["lr_schedule"])
+        LearningRateSchedule.__init__(self, self.config["lr"], self.config["lr_schedule"])
 
         self.loss_in = [
             ("obs", obs_ph),
-            ("value_targets", value_targets_ph),
-            ("advantages", adv_ph),
             ("actions", act_ph),
-            ("logits", logits_ph),
-            ("vf_preds", vf_preds_ph),
+            ("phi1", self.local_ap_network.s1),
+            ("phi2", self.local_ap_network.s2),
+            ("asample", self.local_ap_network.asample),
         ]
-
-        # TODO: testing to see if this lets me pass inputs to ICM
-        # self.variables = ray.experimental.TensorFlowVariables(self.loss_in, self.sess)
 
         TFPolicyGraph.__init__(
             self,
@@ -247,36 +101,32 @@ class PPOPolicyGraphICM(LearningRateSchedule, TFPolicyGraph):
             self.sess,
             obs_input=obs_ph,
             action_sampler=self.sampler,
-            loss=self.loss_obj.loss,
+            loss=self.loss_obj.predloss,
             loss_inputs=self.loss_in,
-            state_inputs=self.model.state_in,
-            state_outputs=self.model.state_out,
-            seq_lens=self.model.seq_lens,
             max_seq_len=config["model"]["max_seq_len"])
 
         self.sess.run(tf.global_variables_initializer())
-        self.explained_variance = explained_variance(value_targets_ph,
-                                                     self.value_function)
+
         self.stats_fetches = {
             "cur_lr": tf.cast(self.cur_lr, tf.float64),
-            "total_loss": self.loss_obj.loss,
-            "policy_loss": self.loss_obj.mean_policy_loss,
-            "vf_loss": self.loss_obj.mean_vf_loss,
-            "vf_explained_var": self.explained_variance,
-            "kl": self.loss_obj.mean_kl,
-            "entropy": self.loss_obj.mean_entropy
+            "total_loss": self.loss_obj.predloss
+            # "policy_loss": self.loss_obj.mean_policy_loss,
+            # "vf_loss": self.loss_obj.mean_vf_loss,
+            # "vf_explained_var": self.explained_variance,
+            # "kl": self.loss_obj.mean_kl,
+            # "entropy": self.loss_obj.mean_entropy
         }
 
     def copy(self, existing_inputs):
         """Creates a copy of self using existing input placeholders."""
-        return PPOPolicyGraphICM(
+        return ICMGraph(
             None,
             self.action_space,
             self.config,
             existing_inputs=existing_inputs)
 
-    def extra_compute_action_fetches(self):
-        return {"vf_preds": self.value_function, "logits": self.logits}
+    # def extra_compute_action_fetches(self):
+    #     return {"vf_preds": self.value_function, "logits": self.logits}
 
     def extra_compute_grad_fetches(self):
         return self.stats_fetches
--- gym_highway/models/model.py
+++ gym_highway/models/model.py
@@ -267,6 +267,10 @@ class StateActionPredictor(object):
         g = tf.nn.relu(linear(g, size, "g1", normalized_columns_initializer(0.01)))
         aindex = tf.argmax(asample, axis=1)  # aindex: [batch_size,]
         logits = linear(g, ac_space, "glast", normalized_columns_initializer(0.01))
+
+        # Required for TFPolicyGraph
+        self.outputs = logits
+
         self.invloss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
                                         logits=logits, labels=aindex), name="invloss")
         self.ainvprobs = tf.nn.softmax(logits, dim=-1)
